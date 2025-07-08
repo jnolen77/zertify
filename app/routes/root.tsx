fcd16e840a190c9511595ff995017f1445e5d6c3
jnolen-mmg <chefjeremynolen@gmail.com>
2025-07-08 09:44:25 -0400

    updating routes layout

---
+++ app/routes/root.tsx
@@ -0,0 +1,43 @@
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+import Header from "~/components/layout/header";
+import Sidebar from "~/components/layout/sidebar";
+import Footer from "~/components/layout/footer";
+import "./app.css";
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-[#fcffe2]">
+        <Header />
+
+        {/* Flex layout for sidebar + main content */}
+        <div className="flex min-h-screen">
+          <Sidebar />
+          <main className="flex-1 p-6">{children}</main>
+        </div>
+
+        <Footer />
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
